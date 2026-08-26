98a742c72b5638ec5e13527c0d6e8f40488bf1c8
ashishmanedeshmukh590 <ashishmanedeshmukh590@gmail.com>
2026-08-26 11:12:30 +0530

    Delete Assignment9 directory

--- Assignment9/question3.py
+++
@@ -1,9 +0,0 @@
-##3.Write a program to reverse a given number using recursive function.
-
-def Reverse(num):
-    if num == 0:
-        return 
-    print(num % 10,end='')
-    Reverse (num // 10)
-num = int(input('Enter the number'))
-Reverse(num)
--- Assignment9/question5.py
+++
@@ -1,8 +0,0 @@
-##5. WAP to find factorial using recusion.
-def factorial(num):
-    if num == 0 or num ==1:
-        return 1
-    return num * factorial (num - 1)
-num = int(input("Eenter value of num: "))
-res = factorial(num)
-print(f'factorial of {num} is {res}')
